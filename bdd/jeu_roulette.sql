e38ed91b5f1ee4143b23afd19ea1fc322f6294af
DylanG-Dev <gametdylan@gmail.com>
2025-04-28 00:02:13 +0200

    Update jeu_roulette.sql

--- bdd/jeu_roulette.sql
+++ bdd/jeu_roulette.sql
@@ -1,21 +1,21 @@
-CREATE DATABASE 'jeu_roulette'
+CREATE DATABASE IF NOT EXISTS jeu_roulette CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE jeu_roulette;
 
-CREATE TABLE `roulette_joueur` (
-  `identifiant` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `nom` varchar(50) NOT NULL,
-  `motdepasse` varchar(255) NOT NULL,
-  `argent` int(11) NOT NULL
+CREATE TABLE IF NOT EXISTS `roulette_joueur` (
+  `identifiant` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `nom` VARCHAR(50) NOT NULL,
+  `motdepasse` VARCHAR(255) NOT NULL,
+  `argent` INT(11) NOT NULL
 );
 
-CREATE TABLE `roulette_partie` (
-  `identifiant` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `joueur` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `mise` int(11) NOT NULL,
-  `gain` int(11) NOT NULL,
-   FOREIGN KEY (joueur) REFERENCES roulette_joueur(identifiant)
+CREATE TABLE IF NOT EXISTS `roulette_partie` (
+  `identifiant` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `joueur` INT(11) NOT NULL,
+  `date` DATETIME NOT NULL,
+  `mise` INT(11) NOT NULL,
+  `gain` INT(11) NOT NULL,
+  FOREIGN KEY (joueur) REFERENCES roulette_joueur(identifiant)
 );
 
-INSERT INTO roulette_joueur VALUES (null, "login", "password", 500);
+INSERT INTO roulette_joueur (nom, motdepasse, argent) VALUES ('login', 'password', 500);
